a503bee7fa7c789c097e4961b6f7921c55f9ef2e
Yar Night <yaroslav239@gmail.com>
2023-08-01 22:33:49 +0300

    feat: end burger-menu

--- library/src/scripts/burger.js
+++ library/src/scripts/burger.js
@@ -1,36 +1,50 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".header__menu");
-console.dir(menu)
+const nav = document.querySelector(".header__nav");
+const logo = document.querySelector(".header__logo");
+const bg = document.querySelector(".bg__header");
+const line1 = document.querySelector(".burger__item1");
+const line2 = document.querySelector(".burger__item2");
+const line3 = document.querySelector(".burger__item3");
 
 let isActive = false; 
 
-function blurBG(isBlured) {
-
-    if(!isBlured){
-        const temp  = document.createElement("DIV");
-        temp.classList.add("blur");
-        document.body.append(temp); 
-    } else{
-        const temp  = document.querySelector(".blur");
-        temp.remove(); 
+ function showMenu(event){
+    if(!isActive){
+        event.stopPropagation()
+        document.body.style.overflowY = "hidden";
+        menu.classList.add("header__menu_active");
+        logo.classList.add("header__logo_active");
+        bg.classList.add("bg__header_active");
+        line1.classList.add("burger__item1_active");
+        line2.classList.add("burger__item2_active");
+        line3.classList.add("burger__item3_active");
+        console.log(logo)
+        isActive = true; 
+    } else {
+        closeMenu(); 
     }
  }
 
- function showMenu(){
-    blurBG(isActive); 
-    document.body.style.overflowY = "hidden";
-    menu.style.overflow = ""; 
-    menu.classList.add(".header__menu_active");
-    isActive = true; 
- }
-
  function closeMenu(){
-    blurBG(isActive); 
     document.body.style.overflowY = "";
-    menu.style.overflow = "hidden"; 
-    menu.classList.remove(".header__menu_active");
+    menu.classList.remove("header__menu_active");
+    logo.classList.remove("header__logo_active");
+    bg.classList.remove("bg__header_active");
+    line1.classList.remove("burger__item1_active");
+    line2.classList.remove("burger__item2_active");
+    line3.classList.remove("burger__item3_active");
+    console.log(`${isActive} close`)
     isActive = false; 
  }
 
+ function navOnClose(event){
+    if(isActive) {
+        closeMenu(); 
+    }  else {
+        return
+    }
+ }
+
  burger.addEventListener("click", showMenu);
- menu.addEventListener("click", closeMenu)
+ nav.addEventListener("click", navOnClose)
